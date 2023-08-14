827da7a636a841b01020302e65d1edef77343155
Begopa <philip0813@naver.com>
2023-08-14 17:22:11 +0900

    feat(all): 정적파일 제공을 위해 express.static 내장 미들웨어 기능 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.use(express.static(path.join(__dirname, "../uploads")));
 
 app.listen(port, () => {
   console.log(`running on ${port} port`);

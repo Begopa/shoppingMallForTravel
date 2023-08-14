116aa08eeee11010450b8456cd13acf325041a22
Begopa <philip0813@naver.com>
2023-08-14 20:43:41 +0900

    feat(database): mongoDB와 연결

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const port = 4000;
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongoDB connection ON");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });

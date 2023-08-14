55f5fd3d25a3d080ceaa3b03189716487cd8b5c3
Begopa <philip0813@naver.com>
2023-08-14 18:56:56 +0900

    feat(all): JSON 데이터 형식을 Javascript로 파싱하기 위한 express.json() 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,13 @@ const cors = require("cors");
 const port = 4000;
 
 app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.use(express.static(path.join(__dirname, "../uploads")));

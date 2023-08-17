3e5c20e5c8cea22c11fa37779c624536f7f31efb
Begopa <philip0813@naver.com>
2023-08-17 14:38:50 +0900

    frontend feat(route): 로그인 상태에 따라 접속 가능한 사이트 제한

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,9 @@ import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { authUser } from "./store/thunkFunctions.js";
+import ProtectedPage from "./pages/ProtectedPage/index.jsx";
+import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
+import NotAuthRoutes from "./components/NotAuthRoutes.jsx";
 function Layout() {
   return (
     <div className="flex flex-col h-screen justify-between">
@@ -41,9 +44,17 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<LandingPage />} />
 
-        <Route path="/login" element={<LoginPage />} />
+        {/*로그인 한 사람만 갈 수 있는 경로*/}
+        <Route element={<ProtectedRoutes isAuth={isAuth} />}>
+          <Route path="/protected" element={<ProtectedPage />} />
+        </Route>
 
-        <Route path="register" element={<RegisterPage />} />
+        {/*로그인 한 사람은 갈 수 없는 경로*/}
+        <Route element={<NotAuthRoutes isAuth={isAuth} />}>
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
       </Route>
     </Routes>
   );
---
+++ frontend/src/pages/ProtectedPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProtectedPage() {
+  return <div>protected page</div>;
+}
+
+export default ProtectedPage;

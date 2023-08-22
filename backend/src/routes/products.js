53d61471f436afe7269fe2b7c8919083fe4aec5b
Begopa <philip0813@naver.com>
2023-08-22 10:21:03 +0900

    backend fix(product): 커밋 코멘트 수정을 위한 삭제

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -24,32 +24,6 @@ router.post("/image", auth, async (req, res, next) => {
   });
 });
 
-router.get("/:id", async (req, res, next) => {
-  const type = req.query.type;
-  let productIds = req.params.id;
-
-  if (type === "array") {
-    // id=32423423423,345345345345345,345345345
-    // productIds = ['32423423423', '345345345345345345', '345345345345345']
-
-    let ids = productIds.split(",");
-    productIds = ids.map((item) => {
-      return item;
-    });
-  }
-
-  // productId를 이용해서 DB에서 productId와 같은 상품의 정보를 가져옵니다.
-  try {
-    const product = await Product.find({ _id: { $in: productIds } }).populate(
-      "writer",
-    );
-
-    return res.status(200).send(product);
-  } catch (error) {
-    next(error);
-  }
-});
-
 router.get("/", async (req, res, next) => {
   // asc 오름차순  , desc 내림차순
   const order = req.query.order ? req.query.order : "desc";
